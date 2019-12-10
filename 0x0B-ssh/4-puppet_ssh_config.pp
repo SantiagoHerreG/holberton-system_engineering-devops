cde09cc8f13c45a70f3511a6d2c1f20bbfb0fa0d
SantiagoHerreG <888@holbertonschool.com>
2019-12-10 17:15:17 +0000

    4-puppet used for configurating the ssh connection

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,12 +1,10 @@
 # Adds lines to the config file for ssh
 
-file_line { 'Turn off passwd auth':
-  path    => '/home/vagrant/.ssh/config',
-  line    => "PasswordAuthentication no\n",
-  require => File['/home/vagrant/.ssh/config'],
-}
 file_line { 'Declare identity file':
-  path    => '/home/vagrant/.ssh/config',
-  line    => "IdentityFile ~/.ssh/holberton\n",
-  require => File['/home/vagrant/.ssh/config'],
+  path => '/home/vagrant/.ssh/config',
+  line => "IdentityFile ~/.ssh/holberton\n",
+}
+file_line { 'Turn off passwd auth':
+  path => '/home/vagrant/.ssh/config',
+  line => "PasswordAuthentication no\n",
 }
