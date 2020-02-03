fe60200767e6a7e509c4d60087f30a016bb1eb4f
SantiagoHerreG <888@holbertonschool.com>
2020-02-03 23:58:58 +0000

    1-export_to_CSV.py: from the data collected using the API a new csv file is generated, username instead of name, pep8

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -15,11 +15,11 @@ todos?userId={}".format(param))
     user = requests.get("http://jsonplaceholder.typicode.com\
 /users/{}".format(param))
 
+    name = user.json().get("username")
     if len(user.json()) and len(todos.json()):
         string = ""
         for to_do in todos.json():
             string += "\"{}\",\"{}\",\"{}\",\"{}\"\n\
-".format(user.json().get("id"), user.json().get("username"),
-         to_do.get("completed"), to_do.get("title"))
+".format(param, name, to_do.get("completed"), to_do.get("title"))
         with open("{}.csv".format(param), mode="w") as f:
             f.write(string)
