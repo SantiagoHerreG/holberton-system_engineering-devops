ada10e090a767118dcef230a17ac9848cbff30e7
SantiagoHerreG <888@holbertonschool.com>
2020-02-03 23:18:16 +0000

    0-gather data: uses a fake API to get data in json format, requests library used

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Write a Python script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+
+    param = sys.argv[1]
+
+    todos = requests.get("http://jsonplaceholder.typicode.com/\
+todos?userId={}".format(param))
+    user = requests.get("http://jsonplaceholder.typicode.com\
+/users/{}".format(param))
+
+    if len(todos.json()) and len(user.json()):
+        total = 0
+        completed = 0
+        comp_list = []
+        for to_do in todos.json():
+            total += 1
+            if to_do.get("completed") is True:
+                completed += 1
+                comp_list.append(to_do.get("title"))
+
+        print("Employee {} is done with tasks({}/{}):\
+".format(user.json().get("name"), completed, total))
+        for task in comp_list:
+            print("\t{}".format(task))
