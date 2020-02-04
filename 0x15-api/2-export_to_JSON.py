ae88947d5cfb8054b1f03e0c1241375d7e44a140
SantiagoHerreG <888@holbertonschool.com>
2020-02-04 00:34:21 +0000

    2-export_to_JSON: takes data input from an API an returns a JSON file

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Write a Python script that takes data from an API
+and creates a JSON file with it
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+
+    param = sys.argv[1]
+
+    todos = requests.get("http://jsonplaceholder.typicode.com/\
+todos?userId={}".format(param))
+    user = requests.get("http://jsonplaceholder.typicode.com\
+/users/{}".format(param))
+
+    name = user.json().get("username")
+    if len(user.json()) and len(todos.json()):
+        string = "{{\"{}\": [".format(param)
+        flag = 0
+        for to_do in todos.json():
+            if flag:
+                string += ", "
+            string += "{{\"username\": \"{}\", \"task\": \"{}\", \"completed\"\
+: {}}}".format(name, to_do.get("title"), to_do.get("completed"))
+            flag = 1
+        string += "]}"
+        with open("{}.json".format(param), mode="w") as f:
+            f.write(string)
