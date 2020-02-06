2db36407bca2780f44615771beaa83b64d4c3cdf
SantiagoHerreG <888@holbertonschool.com>
2020-02-06 17:27:32 +0000

    0-subs: using the Reddit api to retrieve the json data with python requests,  custom use-agent, non-existing subdir handled, allow-redirects false, if not None, executable file, bugs fixed

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,7 +9,10 @@ def number_of_subscribers(subreddit):
     headers = {'User-Agent': "sherre"}
     res = requests.get("https://www.reddit.com/r/" + subreddit + "/about.json",
                        headers=headers, allow_redirects=False)
-    subscribers = res.json().get("data").get("subscribers")
-    if subscribers is not None:
-        return subscribers
-    return 0
+    try:
+        subscribers = res.json().get("data").get("subscribers")
+        if subscribers is not None:
+            return subscribers
+        return 0
+    except:
+        return 0
